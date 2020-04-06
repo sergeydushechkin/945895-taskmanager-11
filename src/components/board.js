13710c373d235d8e0e3640f1e22f86feb47645b3
Sergey <dushechkin_serge@mail.ru>
2020-04-06 16:12:46 +0300

    Разбил проект на модули и подключил bundle.js. Добавил ./ в путях к стилям

---
+++ src/components/board.js
@@ -0,0 +1,8 @@
+export const createSiteBoardTemplate = () => {
+  return (
+    `<section class="board container">
+
+      <div class="board__tasks"></div>
+    </section>`
+  );
+};
---
+++ src/components/learn-more-button.js
@@ -0,0 +1,5 @@
+export const createLearnMoreButtonTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
