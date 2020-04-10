88c3f5bbe7bbd0e80180aeb5073d4e6d05d5664b
Sergey <dushechkin_serge@mail.ru>
2020-04-10 16:25:58 +0300

    Завершены моки генерации задач

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,14 +1,33 @@
 import {getRandomIntegerNumber} from "../utils.js";
 import {COLORS} from "../const.js";
 
+const DefaultRepeatingDays = {
+  "mo": false,
+  "tu": false,
+  "we": false,
+  "th": false,
+  "fr": false,
+  "sa": false,
+  "su": false,
+};
+
 const DESCRIPTIONS = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`];
-const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? -1 : 1;
+  const diffValue = sign * getRandomIntegerNumber(0, 8);
+  targetDate.setDate(targetDate.getDate() + diffValue);
+  return targetDate;
+};
 
 export const generateTask = () => {
+  const dueDate = Math.random() > 0.5 ? null : getRandomDate();
+
   return {
     description: DESCRIPTIONS[getRandomIntegerNumber(0, 2)],
-    date: `23 September`,
-    time: `16:15`,
+    dueDate,
+    repeatingDays: Object.assign({}, DefaultRepeatingDays, {"mo": Math.random() > 0.5}),
     color: COLORS[getRandomIntegerNumber(0, COLORS.length - 1)],
     isArchive: Math.random() > 0.5,
     isFavorite: Math.random() > 0.5
