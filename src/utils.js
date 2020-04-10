87913d9a4a3ddf2145c0aa7adfc12a46434e492e
Sergey <dushechkin_serge@mail.ru>
2020-04-10 16:24:23 +0300

    Переформатирована функция генерации чисел, добавлены функции форматирования времени

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,14 @@
-export const getRandomIntegerNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+export const getRandomIntegerNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const castTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
+export const formatTime = (date) => {
+  const hours = castTimeFormat(date.getHours() % 12);
+  const minutes = castTimeFormat(date.getMinutes());
+
+  return `${hours}:${minutes}`;
+};
